c10100c4e6b89da2f92913488a2c938a89990e24
Kalsito1017 <kurabiika34@abv.bg>
2022-05-02 16:37:14 +0300

    Added Exercise 2

--- 07.AssociativeArraysExercise/02. A Miner Task/Program.cs
+++ 07.AssociativeArraysExercise/02. A Miner Task/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _02._A_Miner_Task
 {
@@ -6,7 +7,24 @@ namespace _02._A_Miner_Task
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string, long> resources = new Dictionary<string, long>();
+            string resource;
+            while ((resource = Console.ReadLine()) != "stop")
+            {
+                int quantity = int.Parse(Console.ReadLine());
+                if (resources.ContainsKey(resource))
+                {
+                    resources[resource] += quantity;
+                }
+                else
+                {
+                    resources.Add(resource, quantity);
+                }
+            }
+            foreach (KeyValuePair<string, long> kvp in resources)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
         }
     }
 }
