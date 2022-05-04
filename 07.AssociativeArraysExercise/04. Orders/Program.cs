bba673f757f2f04f0cf977a8125e683ca44d1e1d
Kalsito1017 <kurabiika34@abv.bg>
2022-05-04 21:54:30 +0300

    Added Exercise 4

--- 07.AssociativeArraysExercise/04. Orders/Program.cs
+++ 07.AssociativeArraysExercise/04. Orders/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _04._Orders
 {
@@ -6,7 +7,42 @@ namespace _04._Orders
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var orders = new Dictionary<string, double>();
+            var newOrders = new Dictionary<string, int>();
+            string input = Console.ReadLine();
+            while (input != "buy")
+            {
+                var token = input.Split();
+                var product = token[0];
+                double price = double.Parse(token[1]);
+                int quantity = int.Parse(token[2]);
+
+                if (!orders.ContainsKey(product))
+                {
+                    orders.Add(product, price);
+                    newOrders.Add(product, quantity);
+                }
+                else if (orders.ContainsKey(product))
+                {
+                    orders.Remove(product);
+                    orders.Add(product, price);
+                    newOrders[product] += quantity;
+
+                }
+                input = Console.ReadLine();
+            }
+            foreach (var order in orders)
+            {
+                foreach (var newOrder in newOrders)
+                {
+                    if (order.Key == newOrder.Key)
+                    {
+                        Console.WriteLine($"{order.Key} -> {order.Value * newOrder.Value:f2}");
+
+                    }
+                }
+
+            }
         }
     }
 }
