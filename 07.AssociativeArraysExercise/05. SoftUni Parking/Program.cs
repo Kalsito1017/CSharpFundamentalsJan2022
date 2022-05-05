7a3f8cd54b756426ebd0889191569c09475c86cc
Kalsito1017 <kurabiika34@abv.bg>
2022-05-05 21:18:23 +0300

    Added Exercise

--- 07.AssociativeArraysExercise/05. SoftUni Parking/Program.cs
+++ 07.AssociativeArraysExercise/05. SoftUni Parking/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _05._SoftUni_Parking
 {
@@ -6,7 +7,46 @@ namespace _05._SoftUni_Parking
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var users = new Dictionary<string, string>();
+            int numberofcommands = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberofcommands; i++)
+            {
+                var comands = Console.ReadLine().Split();
+                var action = comands[0];
+                var userName = comands[1];
+                switch (action)
+                {
+
+                    case "register":
+                        var plateNubmer = comands[2];
+                        if (!users.ContainsKey(userName))
+                        {
+                            users.Add(userName, plateNubmer);
+                            Console.WriteLine($"{userName} registered {plateNubmer} successfully");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"ERROR: already registered with plate number {plateNubmer}");
+                        }
+                        break;
+                    case "unregister":
+                        if (!users.ContainsKey(userName))
+                        {
+                            Console.WriteLine($"ERROR: user {userName} not found");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{userName} unregistered successfully");
+                            users.Remove(userName);
+                        }
+                        break;
+                }
+
+            }
+            foreach (var user in users)
+            {
+                Console.WriteLine($"{user.Key} => {user.Value}");
+            }
         }
     }
 }
