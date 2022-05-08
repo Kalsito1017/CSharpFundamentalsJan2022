2957f29f354dda2cdac8d11c19aa1c713985a0a4
Kalsito1017 <kurabiika34@abv.bg>
2022-05-08 22:51:18 +0300

    Added Exercise 8

--- 07.AssociativeArraysExercise/08. Company Users/Program.cs
+++ 07.AssociativeArraysExercise/08. Company Users/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _08._Company_Users
 {
@@ -6,7 +7,30 @@ namespace _08._Company_Users
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = Console.ReadLine();
+            Dictionary<string, HashSet<string>> results = new Dictionary<string, HashSet<string>>();
+
+            while (input != "End")
+            {
+                string[] inputInfo = input.Split(" -> ");
+                string companyName = inputInfo[0];
+                string personId = inputInfo[1];
+                if (!results.ContainsKey(companyName))
+                {
+                    results.Add(companyName, new HashSet<string>());
+                }
+
+                results[companyName].Add(personId);
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var item in results)
+            {
+                Console.WriteLine($"{item.Key}");
+                Console.WriteLine($"-- {string.Join("\n-- ", item.Value)}");
+            }
+
         }
     }
 }
