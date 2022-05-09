91a0abe0eafad46b157d214652c7194fdf182158
Kalsito1017 <kurabiika34@abv.bg>
2022-05-09 21:50:21 +0300

    Added folder for Text Proccecing Lab exercises

---
+++ 08.TextProcessing - Lab/08.TextProcessing - Lab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _08.TextProcessing___Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
