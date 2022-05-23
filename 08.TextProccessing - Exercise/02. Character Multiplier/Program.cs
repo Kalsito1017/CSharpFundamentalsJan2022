28752dd5efd35b3c25bcf71b8ca74c4fb4f3f011
Kalsito1017 <kurabiika34@abv.bg>
2022-05-23 23:23:46 +0300

    Added Exercise 2

--- 08.TextProccessing - Exercise/02. Character Multiplier/Program.cs
+++ 08.TextProccessing - Exercise/02. Character Multiplier/Program.cs
@@ -6,7 +6,34 @@ namespace _02._Character_Multiplier
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var input = Console.ReadLine().Split();
+            int finalSum = GetStringSum(input[0], input[1]);
+            Console.WriteLine(finalSum);
+
+
+
+        }
+
+        private static int GetStringSum(string stringOne, string stringTwo)
+        {
+            int sum = 0;
+            int minLength = Math.Min(stringOne.Length, stringTwo.Length);
+            for (int i = 0; i < minLength; i++)
+            {
+                sum += stringOne[i] * stringTwo[i];
+            }
+            string longestLengthString = stringOne;
+            if (longestLengthString.Length < stringTwo.Length)
+            {
+                longestLengthString = stringTwo;
+            }
+            for (int i = minLength; i < longestLengthString.Length; i++)
+            {
+                sum += longestLengthString[i];
+            }
+
+
+            return sum;
         }
     }
 }
