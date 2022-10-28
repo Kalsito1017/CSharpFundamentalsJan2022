fd8db47e84caf036b902d4b7c80ceaed25736243
Kalsito1017 <kurabiika34@abv.bg>
2022-10-28 20:48:34 +0300

    Added Regex Exercises

---
+++ Regular Expressions - Exercise/01. Furniture/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _01._Furniture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string regex = @">>(?<name>[A-Za-z\s]+)<<(?<price>\d+(.\d+)?)!(?<quantity>\d+)";
+
+            string input = Console.ReadLine();
+            var list = new List<string>();
+            double totalPrice = 0.0;
+            while (input != "Purchase")
+            {
+                Match match = Regex.Match(input, regex, RegexOptions.IgnoreCase);
+                if (match.Success)
+                {
+                    var name = match.Groups["name"].Value;
+                    var price = double.Parse(match.Groups["price"].Value);
+                    var quantity = int.Parse(match.Groups["quantity"].Value);
+                    list.Add(name);
+                    totalPrice += price * quantity;
+                }
+
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine("Bought furniture:");
+            list.ForEach(Console.WriteLine);
+            Console.WriteLine($"Total money spend: {totalPrice:f2}");
+
+        }
+    }
+}
---
+++ Regular Expressions - Lab/03. Match Dates/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03._Match_Dates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b(\d{2})(\/|\.|-)([A-Z][a-z]{2})\2(\d{4})\b";
+            string text = Console.ReadLine();
+
+            Regex regex = new Regex(pattern);
+            MatchCollection maches = regex.Matches(text);
+
+            foreach (Match match in maches)
+            {
+                Console.WriteLine($"Day: {match.Groups[1].Value}, " +
+                    $"Month: {match.Groups[3].Value}, " +
+                    $"Year: {match.Groups[4].Value}");
+
+            }
+        }
+    }
+}
